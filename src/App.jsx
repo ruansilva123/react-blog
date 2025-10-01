9cc1d9626cd10e0d7c157c23e85d1ef9d6729a43
Ruan Carlos da Silva <ruancplay59@gmail.com>
2025-10-01 12:22:29 -0300

    feat: setup project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
